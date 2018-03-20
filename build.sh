772862d6cea5a5786a41a1a892ce14099aede1eb
ottylab <ottylab@gmail.com>
2018-03-21 01:38:46 +0900

    Support Kitkat

--- build.sh
+++ build.sh
@@ -19,62 +19,70 @@ curl -OL https://github.com/bitzeny/cpuminer/archive/master.zip && \
 
 CXXFLAGS='-std=c++14 -Wno-error=unused-command-line-argument'
 
-for ARCH in arm aarch64 x86 x86_64
+for API in 19 21
 do
-    export TOOLCHAIN=/Android/toolchain-${ARCH}
-    export SYSROOT=$TOOLCHAIN/sysroot
-    export PATH=$PATH:$TOOLCHAIN/bin:$SYSROOT/usr/local/bin
-
-    if [ ${ARCH} == arm ]; then
-        TARGET=arm-linux-androideabi
-    elif [ ${ARCH} == x86 ]; then
-        TARGET=i686-linux-android
+    if [ ${API} -eq 19 ]; then
+        ARCHS=(arm x86)
     else
-        TARGET=${ARCH}-linux-android
+        ARCHS=(arm aarch64 x86 x86_64)
     fi
-    
-    # cross compile environment variables
-    export CC=${TARGET}-clang
-    export CXX=${TARGET}-clang++
-    export AR=${TARGET}-ar
-    export AS=${TARGET}-as
-    export LD=${TARGET}-ld
-    export RANLIB=${TARGET}-ranlib
-    export NM=${TARGET}-nm
-    export STRIP=${TARGET}-strip
-    export CHOST=${TARGET}
-    echo "=================="
-    echo $CC
-    echo "=================="
-    
-    # curl
-    pushd `pwd`
-    cd curl-7.57.0 && ./configure --prefix=$HOME/usr/${ARCH} --host=${TARGET} && \
-        make && make install && make clean
-    popd
-    
-    # jansson
-    pushd `pwd`
-    cd jansson-2.11 && ./configure --prefix=$HOME/usr/${ARCH} --host=${TARGET} && \
-        make && make install && make clean
-    popd
-    
-    # zlib
-    pushd `pwd`
-    cd zlib-1.2.11 && ./configure --prefix=$HOME/usr/${ARCH} && \
-        make && make install && make clean
-    popd
+    for ARCH in ${ARCHS[@]}
+    do
+        export TOOLCHAIN=/Android/toolchain-${API}-${ARCH}
+        export SYSROOT=$TOOLCHAIN/sysroot
+        export PATH=$PATH:$TOOLCHAIN/bin:$SYSROOT/usr/local/bin
 
-    # miner
-    pushd `pwd`
-    cd cpuminer-master && \
-    mkdir -p m4 && cp $HOME/usr/${ARCH}/share/aclocal/libcurl.m4 m4 && \
-        sed -i -e 's/aclocal/aclocal -I m4/' autogen.sh && \
-        echo 'minerd_LDFLAGS += -fPIE -pie' >> Makefile.am && \
-        echo 'minerd_CPPFLAGS += -fPIE' >> Makefile.am && \
-        echo 'ACLOCAL_AMFLAGS = -I m4' >> Makefile.am && \
-        ./autogen.sh && \
-        ./configure --prefix=$HOME/miner/${ARCH} --host=arm-linux-androideabi --with-libcurl=$HOME/usr/${ARCH} && \
-        make && make install && make clean
-    popd
+        if [ ${ARCH} == arm ]; then
+            TARGET=arm-linux-androideabi
+        elif [ ${ARCH} == x86 ]; then
+            TARGET=i686-linux-android
+        else
+            TARGET=${ARCH}-linux-android
+        fi
+
+        # cross compile environment variables
+        export CC=${TARGET}-clang
+        export CXX=${TARGET}-clang++
+        export AR=${TARGET}-ar
+        export AS=${TARGET}-as
+        export LD=${TARGET}-ld
+        export RANLIB=${TARGET}-ranlib
+        export NM=${TARGET}-nm
+        export STRIP=${TARGET}-strip
+        export CHOST=${TARGET}
+        echo "=================="
+        echo $CC
+        echo "=================="
+
+        # curl
+        pushd `pwd`
+        cd curl-7.57.0 && ./configure --prefix=$HOME/usr/${API}/${ARCH} --host=${TARGET} && \
+            make && make install && make clean
+        popd
+
+        # jansson
+        pushd `pwd`
+        cd jansson-2.11 && ./configure --prefix=$HOME/usr/${API}/${ARCH} --host=${TARGET} && \
+            make && make install && make clean
+        popd
+
+        # zlib
+        pushd `pwd`
+        cd zlib-1.2.11 && ./configure --prefix=$HOME/usr/${API}/${ARCH} && \
+            make && make install && make clean
+        popd
+
+        # miner
+        pushd `pwd`
+        cd cpuminer-master && \
+        mkdir -p m4 && cp $HOME/usr/${API}/${ARCH}/share/aclocal/libcurl.m4 m4 && \
+            sed -i -e 's/aclocal/aclocal -I m4/' autogen.sh && \
+            echo 'minerd_LDFLAGS += -fPIE -pie' >> Makefile.am && \
+            echo 'minerd_CPPFLAGS += -fPIE' >> Makefile.am && \
+            echo 'ACLOCAL_AMFLAGS = -I m4' >> Makefile.am && \
+            ./autogen.sh && \
+            ./configure --prefix=$HOME/miner/${API}/${ARCH} --host=${TARGET} --with-libcurl=$HOME/usr/${API}/${ARCH} && \
+            make && make install && make clean
+        popd
+    done
 done
